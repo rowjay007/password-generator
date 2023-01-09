15a2aec1e21be4dbbfe513898966aef3e296a1f9
rowjay007 <rowlives@gmail.com>
2023-01-09 22:57:50 +0100

    add darkmode and password field

---
+++ components/DarkModeSwitch.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+interface DarkModeSwitchProps {
+  onToggle: (darkMode: boolean) => void;
+}
+
+const DarkModeSwitch: React.FC<DarkModeSwitchProps> = ({ onToggle }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className="relative inline-block text-left">
+      <div>
+        <span className="rounded-md shadow-sm">
+          <button
+            type="button"
+            className={`inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out`}
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            {darkMode ? "Dark" : "Light"}
+          </button>
+        </span>
+      </div>
+    </div>
+  );
+};
