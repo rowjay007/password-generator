1daf84c4adba15f6fb4871e65ded8e4be9497265
rowjay007 <rowlives@gmail.com>
2023-01-14 16:24:17 +0100

    remove wrong files

--- app/page.tsx
+++ app/page.tsx
@@ -4,27 +4,12 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
-import Form from "../components/Form";
-import PasswordField from "../components/PasswordField";
-import DarkModeSwitch from "../components/DarkModeSwitch";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [password, setPassword] = useState("");
-  const [darkMode, setDarkMode] = useState(false);
-  const handleGenerate = (length: number, characterTypes: string) => {
-    // Generate password here and set it using setPassword
-  };
-
   return (
-    <div
-      className={`flex flex-col items-center justify-center h-screen bg-${
-        darkMode ? "gray-900" : "gray-100"
-      }`}
-    >
-      <Form onGenerate={handleGenerate} />
-      <PasswordField password={password} />
-      <DarkModeSwitch onToggle={setDarkMode} />
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
